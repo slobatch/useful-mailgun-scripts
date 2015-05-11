a337a8ba5bf19c7d5b5eaad3335d3591f113b9c6
Serge Lobatch <serge@radi.co>
2015-05-11 18:21:56 -0400

    added unsubscribe emails to ignore file. fixed script to include arguments.

--- unsubscribe.sh
+++ unsubscribe.sh
@@ -2,26 +2,29 @@
 
 
 FILE=$1
-KEY=$2
+DOMAIN=$2
+KEY=$3
 
-if [ $FILE != "" ] && [ $KEY != "" ]; then
+
+if [ $FILE != "" ] && [ $KEY != "" ] && [ $DOMAIN != "" ]; then
     echo "Reading file: $FILE"
     echo "Mailgun API Key: $KEY"
 else
-    echo "Expecting a file name AND API Key"
-    echo "e.g. \`unsubscribe.sh file.csv key-00x0xxxxxxxxx000xxx0xxx0xxxxxxx0\`"
+    echo "Expecting a file name AND domain AND API Key"
+    echo "e.g. \`unsubscribe.sh file.csv website.com key-00x0xxxxxxxxx000xxx0xxx0xxxxxxx0\`"
     exit
 fi
 
 function unsubscribe {
 
   counter=0
-  apiArgument='api:'$KEY
+  apiArgument="api:$KEY"
+  echo $apiArgument
   while read p; do
     counter=`expr $counter + 1`
     echo "Unsubscribing email $counter: "
-    curl -s --user 'api:'$KEY \
-      https://api.mailgun.net/v3/props.knotable.com/unsubscribes \
+    curl -s --user $apiArgument \
+      https://api.mailgun.net/v3/$DOMAIN/unsubscribes \
       -F address=$p \
       -F tag='*'
     echo
